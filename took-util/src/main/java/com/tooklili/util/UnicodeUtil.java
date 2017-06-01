b9cd8c637db788f5d427a710a6927b1e4fede289
xxzds <1114332905@qq.com>
2017-06-01 19:57:52 +0800

    unicodeUntil工具类

--- took-util/src/main/java/com/tooklili/util/UnicodeUtil.java
+++ took-util/src/main/java/com/tooklili/util/UnicodeUtil.java
@@ -1,57 +1,22 @@
-package com.tooklili.service.test.dataoke;
+package com.tooklili.util;
 
 import org.apache.commons.lang.StringUtils;
-import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
- * unicode码转换测试
- * 
+ * 字符串和unicode码的转化工具
  * @author shuai.ding
- *
- * @date 2017年5月31日下午5:41:15
+ * @date 2017年6月1日下午7:33:42
  */
-public class UnicodeTest {
+public class UnicodeUtil {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(UnicodeTest.class);
-
-	/**
-	 * unicode编码测试
-	 * 
-	 * @author shuai.ding
-	 */
-	@Test
-	public void unicodeEncodeTest() {
-		String s = "你好";
-		String unicode = string2Unicode(s);
-		LOGGER.info("中文字符串:{}",s);
-		LOGGER.info("编码后的字符串:{}",unicode);
-	}
-
-	/**
-	 * unicode解码测试
-	 * 
-	 * @author shuai.ding
-	 */
-	@Test
-	public void unicodeDecodeTest() {
-		// 在java中"\"是转义字符，所以防止转义，必须写成下面的形式
-		// String s = "\\\u0061\\u0041\\\u4f60\\\u597d\\\u0024\\\u006e\\\u0067\\\u006e\\\u0031";
-		String s = "\\u5168\\u7ad9\\u63a5\\u53e3\\u5df2\\u5347\\u7ea7\\u4e3a\\u5206\\u9875\\u6a21\\u5f0f\\uff0c\\u6bcf\\u987550\\u6761\\u6570\\u636e\\uff0c\\u5206\\u9875\\u53c2\\u6570\\uff1a&page";
-		LOGGER.info("unicode码:{}",s);
-		String str = unicode2String(s);
-		LOGGER.info("转码后的字符串:{}",str);
-	}
-
-	
 	/**
 	 * 将字符串转化成unicode码
+	 * 
 	 * @author shuai.ding
 	 * @param string
 	 * @return
 	 */
-	private String string2Unicode(String string) {
+	public static String string2Unicode(String string) {
 
 		if (StringUtils.isBlank(string)) {
 			return null;
@@ -80,14 +45,14 @@ public class UnicodeTest {
 		return unicode.toString();
 	}
 
-	
 	/**
 	 * 将unicode码转化成字符串
+	 * 
 	 * @author shuai.ding
 	 * @param unicode
 	 * @return
 	 */
-	private String unicode2String(String unicode) {
+	public static String unicode2String(String unicode) {
 		if (StringUtils.isBlank(unicode)) {
 			return null;
 		}
@@ -103,7 +68,7 @@ public class UnicodeTest {
 				sb.append((char) Integer.parseInt(unicode.substring(i + 2, i + 6), 16));
 			}
 		}
-		//如果pos位置后，有非中文字符，直接添加
+		// 如果pos位置后，有非中文字符，直接添加
 		sb.append(unicode.substring(pos));
 
 		return sb.toString();
