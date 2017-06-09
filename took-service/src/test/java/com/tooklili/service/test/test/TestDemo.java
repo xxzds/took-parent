b352109df2d63cc4cda7f2bca2077f4d44dce43c
xxzds <1114332905@qq.com>
2017-06-09 13:31:43 +0800

    提交

---
+++ took-service/src/test/java/com/tooklili/service/test/test/TestDemo.java
@@ -0,0 +1,24 @@
+package com.tooklili.service.test.test;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 测试
+ * @author shuai.ding
+ *
+ * @date 2017年6月8日下午2:16:37
+ */
+public class TestDemo {
+	
+	private static final Logger logger = LoggerFactory.getLogger(TestDemo.class);
+	
+	@Test
+	public void test(){
+		Object object =new Object();
+		logger.info(object.hashCode()+"");
+		
+		logger.info("Str".hashCode()+"");
+	}
+}
