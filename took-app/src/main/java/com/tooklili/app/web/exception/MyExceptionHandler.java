5402bae77c74977fc5a31961c45964c5d5596afe
xxzds <1114332905@qq.com>
2017-08-20 15:26:52 +0800

    异常处理

--- took-app/src/main/java/com/tooklili/app/web/exception/MyExceptionHandler.java
+++ took-app/src/main/java/com/tooklili/app/web/exception/MyExceptionHandler.java
@@ -1,13 +1,11 @@
 package com.tooklili.app.web.exception;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.ModelAndView;
@@ -15,50 +13,42 @@ import org.springframework.web.servlet.ModelAndView;
 import com.alibaba.fastjson.JSON;
 import com.tooklili.util.result.BaseResult;
 import com.tooklili.util.result.CommonResultCode;
+
 /**
  * 自定义异常处理类
+ * 
  * @author shuai.ding
  *
  * @date 2017年2月13日下午2:38:39
  */
 public class MyExceptionHandler implements HandlerExceptionResolver {
-	
-	private static final Logger log = LoggerFactory.getLogger(MyExceptionHandler.class);
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(MyExceptionHandler.class);
 
 	@Override
 	public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object handler,
-			Exception ex) {		
-		BaseResult result = new BaseResult();
-		result.setErrorMessage(CommonResultCode.EXCEPTION,ex.getMessage());
-		this.printWriter(response, JSON.toJSONString(result));
-		
-		//异常信息
-		String errorMesage="";
-		if(handler instanceof HandlerMethod){
-			HandlerMethod handlerMethod =(HandlerMethod)handler;
-			errorMesage="类名："+handlerMethod.getBean().getClass().getName()+",方法名："+handlerMethod.getMethod().getName();
-		}		
-		log.error(errorMesage, ex);
-		return null;
-	}
-	
-	/**
-	 * 将content输出
-	 * @param response
-	 * @param content
-	 * @throws IOException
-	 */
-	private void printWriter(HttpServletResponse response,String content){
-		try {
-//			response.setContentType("text/html;charset=UTF-8"); 
-			response.setContentType("application/json;charset=UTF-8");			
-			PrintWriter out = response.getWriter();
-			out.println(content);
-			out.flush();
-		} catch (IOException e) {
-			log.error("异常：",e);
+			Exception ex) {
+		// 异常信息
+		String errorMesage = "sprngmvc catch exception:";
+		if (handler instanceof HandlerMethod) {
+			HandlerMethod handlerMethod = (HandlerMethod) handler;
+			errorMesage = "类名：" + handlerMethod.getBean().getClass().getName() + ",方法名："
+					+ handlerMethod.getMethod().getName();
 		}
-		
-	}
+		LOGGER.error(errorMesage, ex);
 
+		ModelAndView mv = new ModelAndView();
+		HandlerMethod method = (HandlerMethod) handler;
+		ResponseBody responseBody = method.getMethodAnnotation(ResponseBody.class);
+		if (responseBody != null) {
+			BaseResult result = new BaseResult();
+			result.setErrorMessage(CommonResultCode.EXCEPTION, ex.getMessage());
+			mv.addObject("result", JSON.toJSONString(result));
+			mv.setViewName("/common/jsonException");
+		} else {
+			mv.addObject("exceptionMsg", ex.toString().replaceAll("\n", "<br/>"));
+			mv.setViewName("/common/exception");
+		}
+		return mv;
+	}
 }
