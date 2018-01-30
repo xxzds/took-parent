a7b45bd1a89dfbead0f63c2b2c54925651433ef5
xxzds <1114332905@qq.com>
2018-01-30 09:51:11 +0800

    update .gitignore

---
+++ took-service/src/test/java/com/tooklili/service/es/EsTest.java
@@ -0,0 +1,14 @@
+package com.tooklili.service.es;
+
+import org.junit.Test;
+
+import com.tooklili.service.BaseTest;
+
+public class EsTest extends BaseTest{
+	
+	@Test
+	public void a(){
+		
+	}
+
+}
